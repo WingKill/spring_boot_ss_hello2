e7f69fe3304851fa6157c6b0981d7b644905bf2d
WingKill <zpswlsldi@gmail.com>
2024-04-03 17:25:13 +0900

    프로젝트 생성 / 정적 요소들 넣어보기 / jsp 파일 넣어보기

---
+++ spring_boot_ss_hello2/src/main/java/edu/sejong/ex/SpringBootSsHello2Application.java
@@ -0,0 +1,13 @@
+package edu.sejong.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSsHello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSsHello2Application.class, args);
+	}
+
+}
---
+++ spring_boot_ss_hello2/src/main/java/edu/sejong/ex/controller/HomeController.java
@@ -0,0 +1,25 @@
+package edu.sejong.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/home")
+	public String home() {
+		System.out.println("home()..");
+		return "home";
+	}
+	
+	@GetMapping("/home1")
+	public String home1() {
+		System.out.println("home1()..");
+		return "home1";
+	}
+	
+	@GetMapping("/go")
+	public String go() {
+		System.out.println("go()..");
+		return "go";
+	}
+}
